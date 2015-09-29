b2b6749aa120d7a7f080d7557739da1577cd9a32
unknown <bogdan.cifrovoi@gmail.com>
2015-09-29 23:10:40 +0300

    del

--- dz4/table/contacts.php
+++
@@ -1,6 +0,0 @@
-<head>
-    <body>
-<titel>Мой телефон - 0637363912</titel><br>
-<titel>Почта - bogdan.cifrovoi@gmail.com</titel>
-</body>
-</head>
--- dz4/table/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (empty($_COOKIE['coo_name']) && empty($_COOKIE['coo_email'])){
-    include 'auth-form.php';
-}else{
-    include 'index.php';
-}
-//
-?>
